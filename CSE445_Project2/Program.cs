7f628ce646bedfbadc4030b8415b7d7961f598e1
benrafalski <79751177+benrafalski@users.noreply.github.com>
2021-09-09 14:45:53 -0700

    caught up on case study

--- CSE445_Project2/Program.cs
+++ CSE445_Project2/Program.cs
@@ -8,57 +8,87 @@ namespace CSE445_Project2
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello World!");
-            //Theater show = new Theater();
-            //Thread broker = new Thread(new ThreadStart(show.brokerFunc));
-            
-           //broker.Start();
-            //broker.Name = "broker";
-            
+            Theater theater = new Theater();
+            Thread theaterThread = new Thread(new ThreadStart(theater.theaterFunc));
+            theaterThread.Start();
 
-            TicketBroker broker new TicketBroker();
-            Thread priceModel = new Thread(new ThreadStart(broker.pricingmodel));
-            priceModel.Start();
-
-            Thread order = new Thread(new ThreadStart(broker.orderProcessor));
-            order.Start();
-
-
-
-            Theater.promotion += new promotionalEvent(broker.pro);
+            TicketBroker brokerThread = new TicketBroker(); // subscribe to event
+            Theater.promotion += new promotionalEvent(brokerThread.promotionalEvent);
+            Thread[] brokers = new Thread[2];
+            for(int i = 0; i < 2; i++)
+            {
+                brokers[i] = new Thread(new ThreadStart(brokerThread.brokerFunc));
+                brokers[i].Name = (i + 1).ToString();
+                brokers[i].Start();
+            }
 
 
         }
     }
 
-    public delegate void promotionalEvent();
+    public delegate void promotionalEvent(int price);
 
 
     public class Theater
     {
-        int counter_t = 0;
+        private static int counter_t;
+        private static int ticketPrice;
         public static event promotionalEvent promotion;
-        static Random rng = new Random();
+        static Random rng;
+        
+
         public Theater()
         {
             // constructor
             counter_t = 0;
+            ticketPrice = 100;
+            rng = new Random();
         }
-
-        // method to be started as a thread
-        public void startTheater()
+        public int getPrice() { return ticketPrice; }
+        public static void changePrice(int price)
         {
-            for(; counter_t < 20; counter_t++)
+            if(price < ticketPrice)
             {
-                Thread.Sleep(500);
-
+                if(promotion != null)
+                {
+                    promotion(price);
+                    counter_t = counter_t + 1;
+                }
+                ticketPrice = price;
             }
         }
+
+        public void theaterFunc()
+        {
+            for(int i = 0; i < 50; i++)
+            {
+                Thread.Sleep(500);
+                int p = rng.Next(50, 100);
+                Theater.changePrice(p);
+            }
+        }
+        
+
+        // method to be started as a thread
+        
 
 
 
         // determine ticket prices
-
+        public void PricingModel()
+        {
+            while(counter_t < 20)
+            {
+                int price = rng.Next(40, 200);
+                changePrice(price);
+            }
+        }
         // event stuff
+
+        public void orderProcessing()
+        {
+
+        }
 
         // 
     }
@@ -68,6 +98,23 @@ namespace CSE445_Project2
         public TicketBroker()
         {
             // generates order object
+        }
+
+        public void brokerFunc()
+        {
+            Theater theater = new Theater();
+            for(int i = 0; i < 10; i++)
+            {
+                Thread.Sleep(1000);
+                int p = theater.getPrice();
+                Console.WriteLine("TicketBroker{0} has everyday low price: ${1} each", Thread.CurrentThread.Name, p);
+            }
+
+            
+        }
+        public void promotionalEvent(int p)
+        {
+            Console.WriteLine("TicketBroker{0} tickets are on sale: as low as ${1} each", Thread.CurrentThread.Name, p);
         }
     }
 
